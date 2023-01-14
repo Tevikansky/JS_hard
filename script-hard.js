debefbb570ebf92e79c19111a2b9b56c1191e2b8
Tevikansky <valerabogdanov@inbox.ru>
2023-01-14 17:56:10 +0600

    complete hard task #4

--- script-hard.js
+++ script-hard.js
@@ -1,42 +1,17 @@
 'use strict';
 
-let num = 266219;
-let num2 = num.toString().split('').reduce((a, b) => a * b, 1);
-let num3 = num2 ** 3;
 
-let namePerson = prompt("Введите имя");
-let jobTitle = namePerson === "Артём" ? console.log("директор") : namePerson === "Александр" ? console.log("преподаватель") : console.log("студент");
-
-const days_ru = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
-const days_en = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", " Friday", "Saturday"];
-let myDate = new Date();
-let lang = prompt("ru or en?");
-let Date_ru = "Сегодня: " + days_ru[myDate.getDay()];
-let Date_en = "Today is: " + days_en[myDate.getDay()];
-let ourDate = lang === "ru" ? console.log(Date_ru) : lang === "en" ? console.log(Date_en) : console.log("Error");
-
-
-
-if (lang === "ru") {
-  console.log(Date_ru);
-} else if (lang === "en") {
-  console.log(Date_en);
-} else {
-  console.log("Error");
+const func1 = (str) => {
+  str = prompt('Введите данные', '');
+  str = str.trim();
+  if (!isNaN(str)) {
+    console.log("Это не строка!")
+  } else {
+    if (str.length > 30) {
+      console.log(str.slice(0, 30) + '...')
+    } else {
+      console.log(str)
+    }
+  }
 }
-
-switch (true) {
-  case lang === "ru":
-    console.log(Date_ru);
-    break;
-  case lang === "en":
-    console.log(Date_en);
-    break;
-  default:
-    console.log("Error");
-}
-
-
-
-console.log(num2);
-console.log(num3.toString().substr(0, 2));
+func1()
